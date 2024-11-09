96689278000672d34ba8c90d0555194a2f0f23cb
TechBuckler <fell0000crack0000@proton.me>
2024-11-09 18:08:56 -0500

    Updating stability for build 2

--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mkdir -p "${PLEXGIFMAKER_DATA_PATH:-./plexgifmaker_keys}"
-chmod 755 "${PLEXGIFMAKER_DATA_PATH:-./plexgifmaker_keys}"
-docker-compose down
-docker-compose build
-docker-compose up -d
